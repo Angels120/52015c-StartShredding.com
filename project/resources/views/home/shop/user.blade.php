92e55f51ee41128d7113e1501dc0f65450588766
Russel Smith <russelsmith0125@hotmail.com>
2023-12-03 17:19:12 +0530

    google key update

--- project/resources/views/home/shop/user.blade.php
+++ project/resources/views/home/shop/user.blade.php
@@ -199,7 +199,7 @@
 @section('footer')
 
 <script
-    src="https://maps.googleapis.com/maps/api/js?key=<?=config('GOOGLE_KEY')?>&libraries=places&callback=initMap"
+    src="https://maps.googleapis.com/maps/api/js?key=<?=env('GOOGLE_KEY')?>&libraries=places&callback=initMap"
     async defer></script>
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --}}
 
